a639ea362e676657aee6467f528bc190228dde16
Mamata <ghartimagar@uni-potsdam.de>
2022-12-22 12:56:50 +0100

    still fixing contrast matrix for heatmap

--- linearmodels1.R
+++ linearmodels1.R
@@ -438,20 +438,23 @@ for ( i in upregulated)
 up
 
 # PM1
-#extracting the log fold chnages and pvalues for pm1
+#extracting the log fold changes and pvalues for pm1
+# 
+# lfcpm1 =as.data.frame(summarypm1$test$coefficients)
+# #making 3 columns: each for LL contrast, HL contrast and HL-Ll contrast
+# heat_pm1 = as.data.frame(cbind(lfcpm1[1:95,], lfcpm1[96:190,], lfcpm1[191:285,]))
+# metabolites = pm1$metabolite
+# metabolites = setdiff(metabolites, c("Negative.Control"))
+# rownames(heat_pm1) = metabolites
+# colnames(heat_pm1) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+#pheatmap(heat_pm1, cluster_rows = TRUE, cluster_cols = FALSE)
 
-lfcpm1 =as.data.frame(summarypm1$test$coefficients)
-heat_pm1 = as.data.frame(cbind(lfcpm1[1:95,], lfcpm1[96:190,], lfcpm1[191:285,]))
-metabolites = pm1$metabolite
-metabolites = setdiff(metabolites, c("Negative.Control"))
-rownames(heat_pm1) = metabolites
-colnames(heat_pm1) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
-pheatmap(heat_pm1, cluster_rows = TRUE, cluster_cols = FALSE)
-
+#binding coefficients and p values 
 lfcpm1 =as.data.frame(cbind(summarypm1$test$coefficients, summarypm1$test$pvalues))
 lfcpm1 <- tibble::rownames_to_column(lfcpm1, "contrasts")
 colnames(lfcpm1) = c("contrasts", "lfc", "pval" )
-
+head(lfcpm1)
+#setting all the insignificant p values to NA
 for ( i in 1:nrow(lfcpm1))
 {
   if (lfcpm1$pval[i] >= 0.05)
@@ -461,11 +464,13 @@ for ( i in 1:nrow(lfcpm1))
   }
 }
 
+#taking out LFCs from the second column and arranging them in 3 columns for LL, HL and HL-LL contrast groups
 heat_pm1 = as.data.frame(cbind(lfcpm1[1:95,2], lfcpm1[96:190,2], lfcpm1[191:285,2]))
 metabolites = pm1$metabolite
 metabolites = setdiff(metabolites, c("Negative.Control"))
 rownames(heat_pm1) = metabolites
 colnames(heat_pm1) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+head(heat_pm1)
 #heat_pm1 <- na.omit(heat_pm1)
 #Delete rows with complete NAs
 heat_pm1 <- heat_pm1[rowSums(is.na(heat_pm1)) != ncol(heat_pm1), ]
@@ -694,13 +699,13 @@ dfpm2[226,]
 #set all other LFC values to NA and remove rows(metabolites) that only have NA values). 
 
 # heatmap pm2
-lfcpm2 =as.data.frame(summarypm2$test$coefficients)
-heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,], lfcpm2[96:190,], lfcpm2[191:285,]))
-metabolites = pm2$metabolite
-metabolites = setdiff(metabolites, c("Negative.Control"))
-rownames(heat_pm2) = metabolites
-colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
-pheatmap(heat_pm2, cluster_rows = TRUE, cluster_cols = FALSE)
+# lfcpm2 =as.data.frame(summarypm2$test$coefficients)
+# heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,], lfcpm2[96:190,], lfcpm2[191:285,]))
+# metabolites = pm2$metabolite
+# metabolites = setdiff(metabolites, c("Negative.Control"))
+# rownames(heat_pm2) = metabolites
+# colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+#pheatmap(heat_pm2, cluster_rows = TRUE, cluster_cols = FALSE)
 
 lfcpm2 =as.data.frame(cbind(summarypm2$test$coefficients, summarypm2$test$pvalues))
 lfcpm2 <- tibble::rownames_to_column(lfcpm2, "contrasts")
@@ -725,7 +730,7 @@ colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
 heat_pm2 <- heat_pm2[rowSums(is.na(heat_pm2)) != ncol(heat_pm2), ]
 #heat_pm1<- filter(heat_pm1, rowSums(is.na(heat_pm1)) != ncol(heat_pm1))
 #heat_pm1[is.na(heat_pm1)] <- as.double("NA")
-pheatmap(heat_pm2, na_col="white", cluster_cols = FALSE, cluster_rows = TRUE)
+#pheatmap(heat_pm2, na_col="white", cluster_cols = FALSE, cluster_rows = TRUE)
 
 # llcontrast = as.data.frame(lmpm1$lmlfc[which(as.numeric(lmpm1$indices)<=95)])
 # #take out the signifcant columns and 
@@ -796,14 +801,14 @@ library(stringr)
 
 #outliers in the pm2 dataset
 #smallvariances <-which(pm12var$pm2var.values<2.202078) #170 from her and 22 from outliers. total = 192
-outliers <- boxplot(pm12var$pm2var.values, plot=FALSE)$out
+outliers <- boxplot(pm12var$pm2var.values, plot=FALSE)$out #taking out the outliers
 idx_outliers <- which(pm12var$pm2var.values %in% outliers) #which ones are the outliers
 pm2_outliers <- pm12var[idx_outliers,]
 pm2_outliers <- as.data.frame(pm2_outliers[, c(3,4)])
 #pm2_outliers<- tibble::rownames_to_column(pm2_outliers, "met_indices") 
 
 #sorting the data from highest to lowest variance
-pm2_outliers <- pm2_outliers[order(pm2_outliers$pm2var.values, decreasing = TRUE),]
+pm2_outliers <- pm2_outliers[order(pm2_outliers$pm2var.values, decreasing = TRUE),] #ordering 
 #pm2_outliers<- tibble::rownames_to_column(pm2_outliers, "met_indices") 
 var_mets <- as.character(pm2_outliers$pm2var.group)
 spm2_modified <- spm2
@@ -815,26 +820,74 @@ met_spm2 <- as.character(spm2_modified$group)
 # idx22<- idx2[(length(idx2)-3):length(idx2)]
 # met_spm2[idx22]
 
+#testing
+
+for ( i in 1:length(var_mets))
+{
+  idxes <- which(grepl(var_mets[i], spm2_modified$group)==TRUE)
+  print(idxes)
+  # 1, 8 and 11th entries give 2 groups 
+}
+
+
+var_mets [1]
+idx1 <- which(grepl(var_mets[1], spm2_modified$group)==TRUE)
+spm2_modified[idx1,] # need to excluse the first 3
+
+var_mets [8]
+idx8 <- which(grepl(var_mets[8], spm2_modified$group)==TRUE)
+spm2_modified[idx8,] # need to excluse the last 3
+
+var_mets [11]
+idx11 <- which(grepl(var_mets[11], spm2_modified$group)==TRUE)
+spm2_modified[idx11,]
+#need to exclude the first 3
+
+
 for ( i in 1:length(var_mets))
 {
   barlet <- bartlett.test(values~group, data=spm2_modified)
+  
   pvalue <- barlet$p.value
+  
   if (pvalue <= 0.05)
   {
-    if ( i ==1)
+    if ( i %in% c(1, 11))
     {
-      idx= which(grepl(var_mets[i], spm2_modified$group)==TRUE)
-      idx <- idx[(length(idx)-2):length(idx)]
+      #Grepl() return TRUE if the given pattern is present in the vector. Otherwise, it return FALSE
+      #grepl('pattern', vector)
+      #searching var_met(i) ie each group in the ooutlier in the spm2_modified groups
+      # returns the indices where  we find it
+      idx= which(grepl(var_mets[1], spm2_modified$group)==TRUE)
+      #Butyric.acidLL is the  var_mets[1]
+      # since it returns 3 entries for the other group g.Amino.N.Butyric.acidLL, that has Butyric.acidLL in them
+      # we do the following to exlude first 3 indices
+      idx <- idx[(length(idx)-2):length(idx)] # exclusding 271 273 275 indces
+      
+      # now removing the Butyric.acidLL group 
       spm2_modified = spm2_modified[-c(idx),]
+      # doing the barlett test and taking out the p values
       barlet<- bartlett.test(values~group, data=spm2_modified)
-      pvalue <- barlet$pvalue
+      pvalue <- barlet$p.value
+      
+    }
+    
+    else if ( i ==8)
+    {
+      idx= which(grepl(var_mets[8], spm2_modified$group)==TRUE)
+      idx <- idx[1:3] # exclusding last 3 indices
+      # now removing the Butyric.acidLL group 
+      spm2_modified = spm2_modified[-c(idx),]
+      # doing the barlett test and taking out the p values
+      barlet<- bartlett.test(values~group, data=spm2_modified)
+      pvalue <- barlet$p.value
     }
     else
     {
       idx= which(grepl(var_mets[i], spm2_modified$group)==TRUE)
       spm2_modified = spm2_modified[-c(idx),]
       barlet<- bartlett.test(values~group, data=spm2_modified)
-      pvalue <- barlet$pvalue
+      pvalue <- barlet$p.value
     }
     
   }
@@ -868,14 +921,19 @@ c[162]
 #taking out the column names of the Xpm2 matrix for the metabolite names
 cHL= Xpm2[,161] #negative control HL 
 cLL= Xpm2[,162] # negative controlLL
+
 #to extract the groups that are excluded from spm2 dataset
 setdif<- setdiff(spm2, spm2_modified)
-# 48
+
 expelled_mets <- setdif$group
+length(expelled_mets)
+#54
+#means 54 entries were deleted from spm2_modified which means 54/3 = 18 groups
 
 '%ni%'<- Negate('%in%')
 vec <- vector(mode = "character", length = ncol(Xpm2)/2)
 vec2 <- vector(mode = "character", length = ncol(Xpm2)/2)
+
 for ( i in 1:ncol(Xpm2))
 {
   if (i%%2==0)
@@ -896,6 +954,7 @@ for ( i in 1:ncol(Xpm2))
   }
 }
 length(vec)
+# the empty strings are the expelled groups and we remove them now from our  contrast
 vec = vec[vec != ""]
 length(vec)
 length(vec2)
@@ -903,12 +962,11 @@ length(vec2)
 vec2 = vec2[vec2 != ""]
 length(vec2)
 
+# removing so Negative.Control entries so that we dont get NAs which actually stops glht function from running
 vec = setdiff(vec, c("Negative.ControlLL-Negative.ControlLL"))
-
 vec2 = setdiff(vec2, c("Negative.ControlHL-Negative.ControlHL"))
 
-#to be edited till line 932 
-
+#adding the brackets 
 metabolitevec1 = vector()
 for ( i in 1: length(vec))
 {
@@ -921,8 +979,6 @@ for ( i in 1: length(vec2))
 {
   metabolitevec2[i] = paste( c("("), vec2[i], c(")"))
 }
-
-
 
 
 metabolitevec <- paste(metabolitevec2, metabolitevec1, sep ="-")
@@ -955,18 +1011,15 @@ p <- ggplot(data=lmpm2, aes(x=lmlfc, y=-log10(lmpval), col=diffexpressed)) + geo
 p
 
 # heatmap pm2
-lfcpm2 =as.data.frame(summarypm2$test$coefficients)
-heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,], lfcpm2[96:190,], lfcpm2[191:285,]))
-metabolites = pm2$metabolite
-metabolites = setdiff(metabolites, c("Negative.Control"))
-rownames(heat_pm2) = metabolites
-colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
-pheatmap(heat_pm2, cluster_rows = TRUE, cluster_cols = FALSE)
+# lfcpm2 =as.data.frame(summarypm2$test$coefficients)
+# dim(lfcpm2)
+# dim(lfcpm1)
+# 
 
 lfcpm2 =as.data.frame(cbind(summarypm2$test$coefficients, summarypm2$test$pvalues))
 lfcpm2 <- tibble::rownames_to_column(lfcpm2, "contrasts")
 colnames(lfcpm2) = c("contrasts", "lfc", "pval" )
-
+head(lfcpm2)
 for ( i in 1:nrow(lfcpm2))
 {
   if (lfcpm2$pval[i] >= 0.05)
@@ -975,25 +1028,114 @@ for ( i in 1:nrow(lfcpm2))
     lfcpm2$lfc[i] = NA
   }
 }
+dim(lfcpm2) # 260 instead of 285
 
-heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,2], lfcpm2[96:190,2], lfcpm2[191:285,2]))
-metabolites = pm2$metabolite
-metabolites = setdiff(metabolites, c("Negative.Control"))
-rownames(heat_pm2) = metabolites
-colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+# heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,2], lfcpm2[96:190,2], lfcpm2[191:285,2]))
+# metabolites = pm2$metabolite
+# metabolites = setdiff(metabolites, c("Negative.Control"))
+# rownames(heat_pm2) = metabolites
+# colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+# #heat_pm1 <- na.omit(heat_pm1)
+# #Delete rows with complete NAs
+# heat_pm2 <- heat_pm2[rowSums(is.na(heat_pm2)) != ncol(heat_pm2), ]
+
+#before kicking out the groups we have 285 entries in the groups, and wach contrast column
+# ie. LL, HL and HL-LL contrasts had 95 entries but ow we have 260 entries in the group
+# and the no. of groups in each contrast is not the same 
+# so we cannot use this: heat_pm2 = as.data.frame(cbind(lfcpm2[1:95,], lfcpm2[96:190,], lfcpm2[191:285,]))
+#loop to find out how many LL, HL and HL-LL contrast groups are there after removal of certain groups
+LL = 0
+HL = 0
+HL_LL_diff = 0
+for ( i in 1:nrow(lfcpm2))
+{
+  if (grepl("\\LL$", lfcpm2$contrasts[i])==TRUE)
+  {
+    LL = LL+1
+  }
+  else if  (grepl("\\HL$", lfcpm2$contrasts[i])==TRUE)
+  {
+    HL = HL+1
+  }
+  else
+  {
+    HL_LL_diff = HL_LL_diff+1
+  }
+  
+}
+
+LL
+HL
+HL_LL_diff
+
+HLgroups <- lfcpm2[1:HL,]
+LLgroups<-lfcpm2[85:172,]
+HL_LL_diff_groups <- lfcpm2[173:260,]
+
+# lets add four rows at the bottom so that we can bind them together
+x = matrix(data=NA,nrow=4,ncol=3)
+colnames(x) = c("contrasts", "lfc", "pval" )
+HLgroups <- rbind(HLgroups, x)
+
+#lets check if the dimensions of the contrast columns are equal
+dim(HLgroups) == dim(LLgroups) 
+dim(HLgroups)== dim(HL_LL_diff_groups)
+
+heat_pm2 = as.data.frame(cbind(HLgroups, LLgroups, HL_LL_diff_groups))
+
+
+
+#for rownames 
+
+metabolites = pm2$metabolite #consists of all the metabolites
+# but we have expelled some of them 
+length(metabolites)
+dim(heat_pm2) 
+expelled_mets
+
+#looking at the expelled mets we see that all 6 entries ( ie. 3 samples fom HL and 3 from LL) 
+#of b.D.Allose, L.Glucose, Caproic.acid are included in the expelled_mets, so we can expell them altogether 
+#from our metabolite vector´
+#this gives us the 8 meatabolite names that were expelled including NegativeControlLL and NegativeControlHL
+
+metabolites = setdiff(metabolites, c("Negative.Control", "b.D.Allose", "L.Glucose", "Caproic.acid" ))
+#rownames(heat_pm2) = metabolites
+colnames(heat_pm2) = c("HLgroups", "HL_lfc", "HLpvalues", "LLgroups", "LL_lfc","LLpvalues" , "HL_LL_dif_groups", "HL_LL_dif_lfc", "HL_LL_dif_pvalues")
+head(heat_pm2)
+
+# we can also get the metabolite ames after exclusion from the spm2_modified dataframe # i forgot about it 
+met = unique(spm2_modified$metabolite)
+met = setdiff(met, c("Negative.Control"))
+met == metabolites #true for all
+met = as.data.frame(met)
+
+length(met) # 92, thus there should be 92 entries in the contrast matrix but we have 88
+#now need to compare the metabolite in each row, if they dont match then i set NAs to the positions where they dont
+
+# okay i work with the LLcontrast column because that is the column that differs from the other 2
+# i will extrast the metabolite name from the each row and check if the corresponding rows in 
+#HLcontrast and HL-LL contrast columns have this metabolite, if not i add NAs to those rows
+
+#s you can see the metabolite groups for LLgroups differ from HLgroups and HL_LL_Diff_groups. 
+#i need to come up with function or whatever that extracts the metabolite name excluding "LL" 
+#from "LLgroups" column and see if "HLgroups" and "HL_LL_Diff_groups"  have this metabolite 
+#in the corresponding rows if not i need to add NAs to the those rows so that i have like 
+#for example for second row :  NA   NA   NA   a.Keto.Valeric.acidLL-Negative.ControlLL  -4.299579   1.391480e-04    NA NA NA AND SO ON
+for (i in 1: nrow(heat_pm2))
+{
+  print ("implementation here")
+}
+
+#rownames(heat_pm2) ==met
+
+#edit 1108 to 1113
+
+#rownames(heat_pm2) = metabolites
+#colnames(heat_pm2) = c("LLcontrasts", "HLcontrasts", "HL-LL contrasts")
+
 #heat_pm1 <- na.omit(heat_pm1)
 #Delete rows with complete NAs
-heat_pm2 <- heat_pm2[rowSums(is.na(heat_pm2)) != ncol(heat_pm2), ]
-#heat_pm1<- filter(heat_pm1, rowSums(is.na(heat_pm1)) != ncol(heat_pm1))
-#heat_pm1[is.na(heat_pm1)] <- as.double("NA")
-pheatmap(heat_pm2, na_col="white", cluster_cols = FALSE, cluster_rows = TRUE)
-
-# llcontrast = as.data.frame(lmpm1$lmlfc[which(as.numeric(lmpm1$indices)<=95)])
-# #take out the signifcant columns and 
-# hlcontrast = which(as.numeric(lmpm1$indices)>95 & as.numeric(lmpm1$indices)<=190)
-# hl_ll_diffcontrast = which(as.numeric(lmpm1$indices)>190)
-# 
-# significant_pm1 = as.data.frame(c(llcontrast, hlcontrast, hl_ll_diffcontrast))
+#heat_pm2 <- heat_pm2[rowSums(is.na(heat_pm2)) != ncol(heat_pm2), ]
 
 
 # heat_pm1 and heat_pm2 matrices consist of dataframe of metabolite with significant effect 
@@ -1015,5 +1157,13 @@ Ssuperheat(heat_pm2,
           # change color of missing values
           heat.na.col = "white", heat.col.scheme =  "red")
 
+#ANOVA
+#Null hypothesis: Means of the sample groups are equal
+#Alternative hypothesis: At least the mean of one group is not equal
+#If the null hypothesis is rejected, we need to determine which of the 
+#group means differ from each other.
+
 # STEP 5 : BUILD SUPPORT VECTOR MACHINES AND RANDOM FOREST CLASSIFIERS TO SEPARATE
 #THE METABOLITES WHICH HAVE EFFECT FROM THE ONES WHICH DONOT
+
+
